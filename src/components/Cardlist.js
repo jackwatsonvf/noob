4a14b075de3d886771b91bc8dda6b7bf36ab35ba
Alessandro Giordo <bovas85@gmail.com>
2018-07-12 15:25:27 +0100

    general linting

--- src/components/Cardlist.js
+++ src/components/Cardlist.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import data from './../data.json';
+import React, { Component } from 'react'
+import data from './../data.json'
 
-import Card from './Card';
+import Card from './Card'
 
 const Cardlist = () => {
-	return data.software.map(ting => (
-		<Card 
-			className="card"
-			icon={ting.icon}
-			homepage={ting.website} 
-			title={ting.title} 
-			para1={ting.para1} 
-			para2={ting.para2}
-			download={ting.download}
-			setuppara1={ting.setuppara1}
-			setuplink1={ting.setuplink1}
-			setupimg1={ting.setupimg1}
-			setuppara2={ting.setuppara2}
-			ting={ting} 
-		/>
-	))
+  return data.software.map(ting =>
+    <Card
+      className="card"
+      icon={ting.icon}
+      homepage={ting.website}
+      title={ting.title}
+      para1={ting.para1}
+      para2={ting.para2}
+      download={ting.download}
+      setuppara1={ting.setuppara1}
+      setuplink1={ting.setuplink1}
+      setupimg1={ting.setupimg1}
+      setuppara2={ting.setuppara2}
+      ting={ting}
+    />
+  )
 }
 
 export default Cardlist
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,17 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import introImage from './../img/idk.jpg';
+import introImage from './../img/idk.jpg'
 
 class Intro extends Component {
-	render() {
-		return(
-			<div className="intro">
-				<img src={introImage} />
-				<h2>Welcome to the Vodanoob Portal</h2>
-				<p>Here we've put together a list of some of the software you'll be using, with a brief intro to what it does and how to get it up and running on your machine.</p>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div className="intro">
+        <img src={introImage} />
+        <h2>Welcome to the Vodanoob Portal</h2>
+        <p>
+          Here we've put together a list of some of the software you'll be
+          using, with a brief intro to what it does and how to get it up and
+          running on your machine.
+        </p>
+      </div>
+    )
+  }
 }
 
-export default Intro;
+export default Intro
